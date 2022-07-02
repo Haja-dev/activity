794964f7f8f7d770eec1e562869d1cb609cfb0b3
Haja <hajanemi@gmail.com>
2022-07-02 23:10:09 +0300

    First manual commit: clean up + add logo

---
+++ src/components/WebsiteLogo.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="website-logo">
+    <img src="../assets/logo.svg" alt="Your next activity">
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'WebsiteLogo',
+}
+</script>
+
+<style scoped>
+
+</style>
