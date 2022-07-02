9eece69e89aee0e177dfcedd6ec1a2e2daf8956d
Haja <hajanemi@gmail.com>
2022-07-03 01:08:05 +0300

    Add vuetify + base layout

--- src/App.vue
+++ src/App.vue
@@ -1,29 +1,102 @@
 <template>
-  <div id="app">
-    <WebsiteHeader />
-  </div>
+  <v-app id="inspire">
+    <v-app-bar
+      app
+      color="white"
+      flat
+    >
+      <v-container class="py-0 fill-height">
+        <WebsiteLogo />
+
+        <v-btn
+          v-for="link in links"
+          :key="link"
+          text
+        >
+          {{ link }}
+        </v-btn>
+
+        <v-spacer></v-spacer>
+
+        <v-responsive max-width="260">
+          <v-text-field
+            dense
+            flat
+            hide-details
+            rounded
+            solo-inverted
+          ></v-text-field>
+        </v-responsive>
+      </v-container>
+    </v-app-bar>
+
+    <v-main class="grey lighten-3">
+      <v-container>
+        <v-row>
+          <v-col cols="2">
+            <v-sheet rounded="lg">
+              <v-list color="transparent">
+                <v-list-item
+                  v-for="n in 5"
+                  :key="n"
+                  link
+                >
+                  <v-list-item-content>
+                    <v-list-item-title>
+                      List Item {{ n }}
+                    </v-list-item-title>
+                  </v-list-item-content>
+                </v-list-item>
+
+                <v-divider class="my-2"></v-divider>
+
+                <v-list-item
+                  link
+                  color="grey lighten-4"
+                >
+                  <v-list-item-content>
+                    <v-list-item-title>
+                      Refresh
+                    </v-list-item-title>
+                  </v-list-item-content>
+                </v-list-item>
+              </v-list>
+            </v-sheet>
+          </v-col>
+
+          <v-col>
+            <v-sheet
+              min-height="70vh"
+              rounded="lg"
+            >
+              <!--  -->
+            </v-sheet>
+          </v-col>
+        </v-row>
+      </v-container>
+    </v-main>
+  </v-app>
 </template>
 
 <script>
-import WebsiteHeader from './components/WebsiteHeader.vue'
+import WebsiteLogo from './components/WebsiteLogo.vue';
 
 export default {
   name: 'App',
   components: {
-    WebsiteHeader
-  }
+    WebsiteLogo
+  },
+  data: () => ({
+    links: [
+      'Home',
+      'About'
+    ],
+  }),
 }
 </script>
 
 <style>
-#app {
-  font-family: 'Finlandica', sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-h1, h2, h3, h4 {
-  font-family: 'Holtwood One SC', serif;
+.v-application {
+  font-family: 'Finlandica', sans-serif !important;
 }
 </style>
--- src/components/WebsiteLogo.vue
+++ src/components/WebsiteLogo.vue
@@ -1,6 +1,7 @@
 <template>
   <div class="website-logo">
     <img src="../assets/logo.svg" alt="Your next activity">
+    <span class="website-name">Activity</span>
   </div>
 </template>
 
@@ -11,5 +12,13 @@ export default {
 </script>
 
 <style scoped>
-
+.website-logo {
+  display: flex;
+  align-items: center;
+}
+.website-name {
+  font-family: 'Holtwood One Sc', 'Finlandica', sans-serif;
+  margin-left: 5px;
+  font-size: 27px;
+}
 </style>
